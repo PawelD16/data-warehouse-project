cfc5a444b5e7fc47a6604cecdba09585e1968ae3
PawelD16 <pawel200216@interia.eu>
2024-05-19 15:41:12 +0200

    Fix: ETL AGAIN

--- etl/normalize_blue_bike_data/main.py
+++ etl/normalize_blue_bike_data/main.py
@@ -19,7 +19,7 @@ DEFAULT_CLEANED_SUFFIX = "_cleaned"
 def main(
         taxi_csv_path: str, bike_output: str, bike_csv_paths: List[str]
 ) -> None:
-    if all([os.path.isfile(file)
+    if all([os.path.isfile(add_suffix_to_filename(file, DEFAULT_CLEANED_SUFFIX))
             for file in [bike_output, taxi_csv_path]]):
         return
 
@@ -29,16 +29,16 @@ def main(
     )
 
     remove_commas(bike_df).to_csv(
-        bike_output, index=False
+        add_suffix_to_filename(bike_output, DEFAULT_CLEANED_SUFFIX), index=False
     )
     remove_commas(taxi_df).to_csv(
-        taxi_csv_path, index=False
+        add_suffix_to_filename(taxi_csv_path, DEFAULT_CLEANED_SUFFIX), index=False
     )
 
 
 def read_argv_with_defaults(argv: List[str]) -> Tuple[str, str, List[str]]:
     if len(argv) < 4:
-        return add_suffix_to_filename(DEFAULT_TAXI_CSV_PATH, DEFAULT_CLEANED_SUFFIX), add_suffix_to_filename(DEFAULT_CSV_OUTPUT, DEFAULT_CLEANED_SUFFIX), DEFAULT_BIKE_CSV_PATHS
+        return DEFAULT_TAXI_CSV_PATH, DEFAULT_CSV_OUTPUT, DEFAULT_BIKE_CSV_PATHS
 
     return argv[1], argv[2], argv[3:]
 
